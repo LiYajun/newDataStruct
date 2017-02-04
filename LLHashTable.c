f630a0dacc4a3ab969bbc6f2339f81c66d351ae8
ANDYBAIN-COMPUT\andybain <283284029@qq.com>
2017-02-05 00:04:22 +0800

    crate LLHashTable

---
+++ LLHashTable.c
@@ -0,0 +1,28 @@
+//
+//  LLArray.c
+//  newDataStructPro
+//
+//  Created by li yajun on 17/1/4.
+//  Copyright © 2017年 li yajun. All rights reserved.
+
+#include "LLHashTable.h"
+#include "mem_allocator.h"
+#include <assert.h>
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ LLHashTable.h
@@ -0,0 +1,25 @@
+//
+//  LLArray.h
+//  newDataStructPro
+//
+//  Created by li yajun on 17/1/4.
+//  Copyright © 2017年 li yajun. All rights reserved.
+//
+
+#ifndef LLHashTable_H
+#define LLHashTable_H
+
+
+#include <stdio.h>
+#include "LLTypes.h"
+#include "LLRef.h"
+
+
+typedef struct _LLHash{
+    LLRef      ref;                 /* 引用计数 */
+
+}LLHashTable;
+ 
+
+
+#endif /* LLHashTable_H */
