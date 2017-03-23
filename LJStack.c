30b4fefc944ed161f14d4165a0e2b23d80bcd1d1
ANDYBAIN-COMPUT\andybain <283284029@qq.com>
2017-03-23 23:26:29 +0800

    LJStack

---
+++ LJStack.c
@@ -0,0 +1,26 @@
+#include "LJStack.h"
+
+
+
+
+
+extern LJStack * LJStackCreateByCap(sint cap)
+{
+	return LLArrayCreateByCap(cap);
+}
+extern void  LJStackPush(LJStack * p, LLRefPtr obj)
+{
+	LLArrayAddObject(p, obj);
+}
+extern LLRefPtr LJStackPop(LJStack * p)
+{
+	if (p->objSize <= 0)
+		return NULL;
+
+	LLRefPtr obj = LLArrayGetObjectAt(p, p->objSize-1);
+	LLRefRetain(obj);
+	LLArrayRemoveAt(p, p->objSize - 1);
+	return obj;
+}
+
+
